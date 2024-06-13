90ab7d3a82c39f2ac2df9b1fdca6ab59059ef661
Alberto Fanton <alberto.fanton@protonmail.com>
2024-06-13 17:16:03 +0200

    Add support for ghc810

--- nix/project.nix
+++ nix/project.nix
@@ -1,7 +1,18 @@
 final: _prev: {
-  ytxp-sdk = final.haskell-nix.cabalProject' {
+  ytxp-sdk-96 = final.haskell-nix.cabalProject' {
     compiler-nix-name = "ghc96";
-    src = final.lib.cleanSource ./../ytxp-sdk;
+    src = final.lib.cleanSource ./..;
+    shell = import ./shell.nix {
+      flake = final.flake;
+      pkgs = final;
+    };
+    inputMap = {
+      "https://chap.intersectmbo.org/" = final.inputs.CHaP;
+    };
+  };
+  ytxp-sdk-810 = final.haskell-nix.cabalProject' {
+    compiler-nix-name = "ghc810";
+    src = final.lib.cleanSource ./..;
     shell = import ./shell.nix {
       flake = final.flake;
       pkgs = final;
