1e21521303fa55118ec26e8ff322ec03887b5291
Giovanni Garufi <nazrhom@gmail.com>
2024-06-20 12:05:25 +0200

    Add PlutusTx.Eq instances to YieldingRedeemer

--- ytxp-sdk/src/Cardano/YTxP/SDK/Redeemers.hs
+++ ytxp-sdk/src/Cardano/YTxP/SDK/Redeemers.hs
@@ -5,10 +5,11 @@ module Cardano.YTxP.SDK.Redeemers (AuthorisedScriptIndex (AuthorisedScriptIndex)
 import Cardano.YTxP.SDK.Vendored (EnumIsData (EnumIsData))
 import GHC.Generics (Generic)
 import PlutusTx qualified
+import PlutusTx.Prelude qualified as PlutusTx
 
 -- | Represents an index into a authorised reference script in a TxInReferenceInput list
 newtype AuthorisedScriptIndex = AuthorisedScriptIndex Integer
-  deriving newtype (Show, Eq, PlutusTx.ToData, PlutusTx.FromData, PlutusTx.UnsafeFromData)
+  deriving newtype (Show, Eq, PlutusTx.ToData, PlutusTx.FromData, PlutusTx.UnsafeFromData, PlutusTx.Eq)
 
 {- The type of yielded to scripts
 -}
@@ -18,10 +19,17 @@ data AuthorisedScriptPurpose = Minting | Spending | Rewarding
     (PlutusTx.ToData, PlutusTx.FromData, PlutusTx.UnsafeFromData)
     via (EnumIsData AuthorisedScriptPurpose)
 
+instance PlutusTx.Eq AuthorisedScriptPurpose where
+  {-# INLINEABLE (==) #-}
+  Minting == Minting = True
+  Spending == Spending = True
+  Rewarding == Rewarding = True
+  _ == _ = False
+
 {- Index for the yielding redeemer
 -}
 newtype AuthorisedScriptProofIndex = AuthorisedScriptProofIndex (AuthorisedScriptPurpose, Integer)
-  deriving newtype (Show, Eq, PlutusTx.ToData, PlutusTx.FromData, PlutusTx.UnsafeFromData)
+  deriving newtype (Show, Eq, PlutusTx.ToData, PlutusTx.FromData, PlutusTx.UnsafeFromData, PlutusTx.Eq)
 
 {- | The redeemer passed to the yielding minting policy, validator,
 and staking validators
@@ -33,5 +41,10 @@ data YieldingRedeemer = YieldingRedeemer
   -- ^ A tuple containing yielded to script type and the index at which to find proof: this allows us to avoid having to loop through inputs/mints/withdrawls to find the script we want to ensure is run.
   }
   deriving stock (Show, Generic, Eq)
+
+instance PlutusTx.Eq YieldingRedeemer where
+  {-# INLINEABLE (==) #-}
+  YieldingRedeemer si spi == YieldingRedeemer si' spi' =
+    si PlutusTx.== si' PlutusTx.&& spi PlutusTx.== spi'
 
 PlutusTx.makeIsDataIndexed ''YieldingRedeemer [('YieldingRedeemer, 0)]
