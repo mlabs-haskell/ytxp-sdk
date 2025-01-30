55631bd4f2655f856a2a74d7dd5df777c466ab7c
Alberto Flores <blackbin@posteo.org>
2025-01-30 13:40:38 -0600

    Fix the formatting

--- nix/checks.nix
+++ nix/checks.nix
@@ -2,15 +2,15 @@
   imports = [
     ./pre-commit.nix
   ];
-    perSystem = { lib, config, pkgs, ... }: {
+  perSystem = { lib, config, pkgs, ... }: {
     checks = {
       # TODO add tests
       run-serialization-test-810 = pkgs.runCommandNoCC "run-serialization-test-810" { } ''
-        ${lib.getExe config.packages.ytxp-lib-810-test} && touch $out
-	'';
+                ${lib.getExe config.packages.ytxp-lib-810-test} && touch $out
+        	'';
       run-serialization-test-96 = pkgs.runCommandNoCC "run-serialization-test-96" { } ''
         ${lib.getExe config.packages.ytxp-lib-96-test} && touch $out
-      '';	
+      '';
     };
   };
 }
