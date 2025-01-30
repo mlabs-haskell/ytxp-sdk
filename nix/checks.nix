9c3359113f22f0b402f45b84d1992b6c7338042a
Alberto Flores <blackbin@posteo.org>
2025-01-30 13:16:47 -0600

    Fix the package name for tests

--- nix/checks.nix
+++ nix/checks.nix
@@ -5,9 +5,14 @@
     perSystem = { lib, config, pkgs, ... }: {
     checks = {
       # TODO add tests
-      run-serialization-test = pkgs.runCommandNoCC "run-serialization-test" { } ''
-        ${lib.getExe config.packages."ytxp-sdk:test:ytxp-sdk-test"} && touch $out
-      '';
+      run-serialization-test-810 = pkgs.runCommandNoCC "run-serialization-test-810" { } ''
+        ${lib.getExe config.packages.ytxp-lib-810-test} && touch $out
+	'';
+      run-serialization-test-96 = pkgs.runCommandNoCC "run-serialization-test-96" { } ''
+        ${lib.getExe config.packages.ytxp-lib-96-test} && touch $out
+      '';	
     };
   };
 }
+
+
