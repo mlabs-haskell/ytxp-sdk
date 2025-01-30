7789476bb1dfc268a0d084d5e1f11de72dec3f48
Alberto Flores <blackbin@posteo.org>
2025-01-30 13:46:08 -0600

    ADDED: treefmt-nix for formatting

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Yielding Transaction Pattern SDK";
   inputs = {
+    treefmt-nix.url = "github:numtide/treefmt-nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskellNix.url = "github:input-output-hk/haskell.nix";
     iohk-nix.url = "github:input-output-hk/iohk-nix";
@@ -10,6 +11,7 @@
       flake = false;
     };
     git-hooks.url = "github:cachix/git-hooks.nix";
+    flake-root.url = "github:srid/flake-root";
   };
 
   outputs = inputs:
@@ -17,6 +19,8 @@
       debug = true;
       systems = [ "x86_64-linux" ];
       imports = [
+        inputs.treefmt-nix.flakeModule
+        ./nix/treefmt.nix
         ./nix/pre-commit.nix
         ./nix/packages.nix
         ./nix/checks.nix
