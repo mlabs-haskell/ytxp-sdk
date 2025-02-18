7ab17f19f3ef47006493188480dae0dbfb71ac19
Alberto Flores <alberto@mlabs.city>
2025-02-18 14:34:12 -0600

    Test current branch for github pages

--- nix/gh-pages.nix
+++ nix/gh-pages.nix
@@ -2,7 +2,7 @@
   imports = [
     inputs.hercules-ci-effects.flakeModule
   ];
-  hercules-ci.github-pages.branch = "master";
+  hercules-ci.github-pages.branch = "alberto/serve-docs";
   perSystem = { config, ... }: {
     hercules-ci.github-pages.settings.contents = config.packages.ytxp-lib-96-docs + "/share/doc";
   };
